8e2d7c8684356268e0eabb90f816452ccd7a0f34
Omie Sawie <omkarsawant20HG@gmail.com>
2024-08-13 01:41:53 +0530

    Changed template

---
+++ components/about.tsx
@@ -0,0 +1,50 @@
+"use client";
+
+import React from "react";
+import SectionHeading from "./section-heading";
+import { motion } from "framer-motion";
+import { useSectionInView } from "@/lib/hooks";
+
+export default function About() {
+  const { ref } = useSectionInView("About");
+
+  return (
+    <motion.section
+      ref={ref}
+      className="mb-28 max-w-[45rem] text-center leading-8 sm:mb-40 scroll-mt-28"
+      initial={{ opacity: 0, y: 100 }}
+      animate={{ opacity: 1, y: 0 }}
+      transition={{ delay: 0.175 }}
+      id="about"
+    >
+      <SectionHeading>About Me</SectionHeading>
+
+      <p className="mb-3">
+        I am currently in my final year of Civil Engineering at{" "}
+        <span className="font-medium">IIT Bhubaneswar</span>. My journey into
+        programming began in the early days of my college experience, where I
+        quickly realized the profound impact that technology could have.
+        Starting with robotics, I soon expanded my skills to encompass{" "}
+        <span className="font-medium">full-stack web development</span>.{" "}
+        <span className="italic">What I find most rewarding</span> about
+        programming is the process of bringing a project to life, and the
+        satisfaction that comes from solving complex challenges. My primary
+        technical stack includes{" "}
+        <span className="font-medium">
+          Node.js, React, ExpressJS, Next.js, and MongoDB
+        </span>
+        , with solid expertise in both NoSQL databases like MongoDB and SQL
+        databases such as MySQL and PostgreSQL. I am also proficient in
+        TypeScript, and I continuously seek to broaden my knowledge and stay
+        abreast of new advancements in technology.
+      </p>
+      <p>
+        <span className="italic">Beyond coding</span>, I enjoy activities that
+        allow me to unwind and recharge. These include playing video games,
+        watching films, cycling, and embarking on long bike rides. In the
+        digital world, I am known as{" "}
+        <span className="font-medium">Omie Sawie</span>.
+      </p>
+    </motion.section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,94 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Summer Robotics Intern, eYantra, IIT Bombay",
+    location: "IIT Bombay, Mumbai, India",
+    description:
+      "After finishing 6th in eYantra Robotics Competition (2021-22), a national level Robotics Competition hosted by IIT Bombay, I recieved an internship offer from eYantra to develop their autonomous robot",
+    icon: React.createElement(LuGraduationCap),
+    date: "May 2022",
+  },
+  {
+    title: "Full-Stack Developer intern",
+    location: "DATOMS, Banglore, India",
+    description:
+      "I worked as a full-stack developer for 2 months in DATOMS Banglore office. My task was to make a completely autonomous, easy to customize and generic data display framework for my clients. The final framework developed was hyper performant and successfully shipped to the clients.",
+    icon: React.createElement(CgWorkAlt),
+    date: "May - July 2024",
+  },
+] as const;
+
+export const projectsData = [
+  {
+    title: "CorpComment",
+    description:
+      "I worked as a full-stack developer on this startup project for 2 years. Users can give public feedback to companies.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind", "Prisma"],
+    imageUrl: corpcommentImg,
+  },
+  {
+    title: "rmtDev",
+    description:
+      "Job board for remote developer jobs. I was the front-end developer. It has features like filtering, sorting and pagination.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: rmtdevImg,
+  },
+  {
+    title: "Word Analytics",
+    description:
+      "A public web app for quick analytics on text. It shows word count, character count and social media post limits.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: wordanalyticsImg,
+  },
+] as const;
+
+export const skillsData = [
+  "HTML",
+  "CSS",
+  "JavaScript",
+  "TypeScript",
+  "React",
+  "Next.js",
+  "Node.js",
+  "Git",
+  "Tailwind",
+  "Prisma",
+  "MongoDB",
+  "Redux",
+  "GraphQL",
+  "Apollo",
+  "Express",
+  "PostgreSQL",
+  "Python",
+  "Django",
+  "Framer Motion",
+] as const;
